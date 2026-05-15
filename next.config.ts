4554b42c8708b7624c9de495c2da791f7a986442
Joshua Babatunde <jdbabatunde98@gmail.com>
2026-05-15 14:53:26 +0100

    updated rewrite rule

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ['10.226.11.160'],
+  allowedDevOrigins: ["10.226.11.160"],
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://4.255.4.79:8080/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
