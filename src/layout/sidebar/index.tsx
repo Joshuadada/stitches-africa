5a2221de4f1d14d86c596d1334c8b790628c7a75
Joshua Babatunde <jdbabatunde98@gmail.com>
2026-05-30 13:19:06 +0100

    updated vendor UI screen conversion

--- src/layout/sidebar/index.tsx
+++ src/layout/sidebar/index.tsx
@@ -56,7 +56,9 @@ const Sidebar = ({ routes, open, onClose }: { routes?: RouteItem[]; open?: boole
 
             <div className="flex flex-col gap-1 md:gap-1.5 lg:gap-2 px-3 sm:px-5 md:px-7 lg:px-9 py-2 sm:py-3 md:py-3.5 lg:py-4 border-y border-[#FAF7F2]/30">
               <h4 className="text-base md:text-lg lg:text-xl font-bold text-white">ADIRE COUTURE</h4>
-              <p className="text-[10px] md:text-xs lg:text-sm text-[#B5894A] font-bold">Gold tier</p>
+              <div className="flex">
+                <p className="text-[10px] md:text-xs lg:text-sm text-[#92400E] font-bold bg-[#FEF3C7] px-4 md:px-5 lg:px-6 py-1 md:py-1.5 lg:py-2 border border-[#F59E0B] rounded-[20px]">Gold tier</p>
+              </div>
             </div>
           </div>
 
